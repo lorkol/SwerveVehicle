7811e7fce7e8c1f77dc66f4f0fc12f6c08fb5b25
lorkol <37380984+lorkol@users.noreply.github.com>
2025-09-25 19:09:46 -0400

    Change numpy typing library
    Changed the library used for typing in numpy to one that also works with python3.13

--- SwerveRobot.py
+++ SwerveRobot.py
@@ -1,5 +1,5 @@
 import numpy as np
-from nptyping import NDArray, Shape, Float64
+from numpy.typing import NDArray
 
 class SwerveRobot:
     def __init__(self, l: float, d: float, r: float):
@@ -10,13 +10,13 @@ class SwerveRobot:
         self.r = r
         '''Robot wheel radius'''
 
-    def create_A_Matrix(self, phi1: float, phi2: float, phi3: float, phi4: float) -> NDArray[Shape["3, 4"], Float64]:
+    def create_A_Matrix(self, phi1: float, phi2: float, phi3: float, phi4: float) -> NDArray[np.float64]:
         phi1 = np.radians(phi1)
         phi2 = np.radians(phi2)
         phi3 = np.radians(phi3)
         phi4 = np.radians(phi4)
 
-        A: NDArray[Shape["3, 4"], Float64] = np.array([
+        A: NDArray[np.float64] = np.array([
             [np.cos(phi1), np.sin(phi1), self.l * np.sin(phi1) + self.d * np.cos(phi1)],
             [np.cos(phi2), np.sin(phi2), self.l * np.sin(phi2) - self.d * np.cos(phi2)],
             [np.cos(phi3), np.sin(phi3), -self.l * np.sin(phi3) - self.d * np.cos(phi3)],
